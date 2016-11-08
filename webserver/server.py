61cae57c90962f669b82bcc38d48e62feec47a76
zst2821045 <zst2821045@gmail.com>
2016-11-08 00:15:46 -0400

    Update server.py

--- webserver/server.py
+++ webserver/server.py
@@ -20,7 +20,9 @@ from sqlalchemy import *
 from sqlalchemy.pool import NullPool
 from flask import Flask, request, render_template, g, redirect, Response
 
-tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
+#tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
+tmpl_dir=os.path.join('templates')
+
 app = Flask(__name__, template_folder=tmpl_dir)
 
 
